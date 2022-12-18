c4dae6f9baa3b0ac32316e78d4737e6e6391ba7f
Root <admin@example.com>
2022-12-18 14:29:12 -0500

    Adds a default config option for the notification sounds

--- public/chat.js
+++ public/chat.js
@@ -890,7 +890,7 @@ function playSound() {
 function bindSounds(){
   // used to bind the sound sources to prevent them from playing intially
   // should look for a setting in storage to enable quad via /quad
-  document.getElementById("audio2").src="https://www.myinstants.com/media/sounds/kitty-blabla.mp3";
+  document.getElementById("audio2").src=chatConfig.DEFAULT_S;
   try{
       var quad_string =  window.localStorage.getItem('useQuad');
           if (quad_string === '1'){
