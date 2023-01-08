c6791737e0cc96e4fd214f195bb7b1b9376eaa71
Admin <admin@admin.com>
2023-01-08 15:19:32 -0500

    Trying some more fixes for user list.

--- index.mjs
+++ index.mjs
@@ -616,7 +616,7 @@ username: user.username,
         //fatchatUserList.push({username:user,avatar:'',color:'',watching:'Playlistbot9k'});
         //fatchatUserList.push({username:user,watching:'Playlistbot9k',data:'something'});
         //fatchatUserList.push({ [user]: {data:{watching:{ page: 'Playlistbot9k' },avatar:null,username:user,page:'Playlistbot9k',color:null}} });
-        viewersList.push({username:user,watching:'Playlistbot9k'});
+        viewersList.push({data:{username:user,watching:'Playlistbot9k',page:'Playlistbot9k'}});
         //fatchatUserList.push([{[user]:{channel:'Playlistbot9k',viewers:viewersList,viewCount:tempnamelist.length}}]);  
         // the other model similar to this is in plb
       });
